810ffadc46bcae6fbf5a3e9d33284b82435a9518
OmniaSamy <omnia.samyabdelmoneam@gmail.com>
2025-08-14 21:11:16 +0300

    Initial Xcode project commit

---
+++ thmanyah_task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  thmanyah_task
+//
+//  Created by Omnia Samy on 14/08/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
