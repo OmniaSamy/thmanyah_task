019b514a87a4a2b9a2668d132ea9a433794dd02a
OmniaSamy <omnia.samyabdelmoneam@gmail.com>
2025-08-15 15:57:25 +0300

    Add app structure - add show error message layer using swiftmessages

--- thmanyah_task/Scenes/Home/HomeViewModel.swift
+++ thmanyah_task/Scenes/Home/HomeViewModel.swift
@@ -11,3 +11,28 @@ class HomeViewModel: ObservableObject {
     
     
 }
+
+extension HomeViewModel {
+    
+    func getHomeList(page: Int = 1) {
+        
+        //  isLoading = true
+        NetworkManager.shared.getHomeList(page: page,
+                                          completion: { [weak self] (result: Result<NetworkResponse, NetworkError>, _) in
+            // self?.isLoadingMore = false
+            switch result {
+            case .success(let data):
+                print("data \(data)")
+                
+                
+                
+            case .failure(let error):
+                print("error \(error)")
+                AppAlertManager.showError(message: error.errorMessage())
+                
+                
+            }
+            
+        })
+    }
+}
